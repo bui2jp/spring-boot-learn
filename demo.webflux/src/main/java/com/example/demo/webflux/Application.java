f241ba4039f7d8f597ed61ee3083a653d40ea0c3
takuya <>
2022-04-13 17:28:50 +0900

    1st

---
+++ demo.webflux/src/main/java/com/example/demo/webflux/Application.java
@@ -0,0 +1,26 @@
+package com.example.demo.webflux;
+
+import com.example.demo.webflux.hello.GreetingClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+//@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+		GreetingClient greetingClient = context.getBean(GreetingClient.class);
+		System.out.println(">> message4fds = " + greetingClient.getMessage().block());
+	}
+
+//	@GetMapping("/hello")
+//	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+//		return String.format("Hello %s!", name);
+//	}
+}
---
+++ demo.webflux/src/test/java/com/example/demo/webflux/ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("start conextLoads().");
+	}
+
+}
